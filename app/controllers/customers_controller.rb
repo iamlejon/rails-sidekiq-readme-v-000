de68238cfe59adc3425b4e752858d4cbbee60206
Dominique McCloud <niquelejon@gmail.com>
2017-10-28 17:02:05 +0000

    automatically backed up by learn

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,7 +4,7 @@ class CustomersController < ApplicationController
   def index
      @customers = Customer.all
    end
-  
+
    def upload
      LeadsWorker.perform_async(params[:leads].path)
      redirect_to customers_path
